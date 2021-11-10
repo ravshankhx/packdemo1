c4d304448f1c18f3594b00f2e4d53b5cfae359c7
ravshankhx <ravshankhxapple@gmail.com>
2021-11-10 16:52:46 +0300

    Initial commit

---
+++ lib/packages/home_page.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  void fireToast(){
+    Fluttertoast.showToast(
+        msg: "This is Center Short Toast",
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.TOP,
+        timeInSecForIosWeb: 1,
+        backgroundColor: Colors.red,
+        textColor: Colors.white,
+        fontSize: 16.0
+    );
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("pakages"),
+      ),
+      body: Center(
+        child: FlatButton(
+          color: Colors.green,
+          onPressed: (){
+            fireToast();
+
+          }, child: Text("home"),
+
+        ),
+      ),
+    );
+  }
+}
